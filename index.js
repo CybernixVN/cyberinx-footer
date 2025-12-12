0d8f88d09a2b677af4386ab50e40ae5506fc8a09
Moji <lephambinh05@gmail.com>
2025-12-12 12:42:24 +0700

    feat: add link to https://cybernix.vn in Footer

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ function Footer() {
     {
       className: "py-4 text-center opacity-70 text-sm",
     },
-    "© 2025 Cybernix.vn – All rights reserved."
+    React.createElement(
+      "a",
+      {
+        href: "https://cybernix.vn",
+        target: "_blank",
+        rel: "noopener noreferrer",
+        style: { color: "inherit", textDecoration: "none" },
+      },
+      "© 2025 Cybernix.vn – All rights reserved."
+    )
   );
 }
 
